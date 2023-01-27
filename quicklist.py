537e789dab5fd1b2a874c7720006e8c22d073521
jussiiih <107620116+jussiiih@users.noreply.github.com>
2023-01-27 15:31:16 +0200

    Update quicklist.py

--- quicklist.py
+++ quicklist.py
@@ -1,33 +1,25 @@
-from collections import deque
+#TEHTÄVÄNANTO
+#Tehtäväsi on toteuttaa oma tehokas lista-tietorakenne, joka tarjoaa seuraavat toiminnot:
+#Poista listalta listan k ensimmäistä alkiota ja siirrä ne samassa järjestyksessä listan loppuun
+#Ilmoita, mikä alkio listalta löytyy indeksiltä i
+
+#Tietorakenteen jokaisen operaation tulee olla O(1)-aikainen. Voit olettaa, että jokainen alkio on kokonaisluku väliltä 1…109.
+#Toteuta tiedostoon quicklist.py luokka QuickList seuraavan mallin mukaisesti.
 
 class QuickList:
     def __init__(self, t):
-        lista = deque()
-        for i in range (0, len(t)):
-            lista.append(t[i])
-        t =lista
         self.t = t
-
+        self.laskuri = 0
+ 
     def move(self, k):
-        for i in range(0, k):
-            self.t[0]=self.t[-1]
-            self.t.popleft()
-            #otettu = self.t.popleft()
-            #self.t.append(otettu)
-
+        k = k % len(self.t)
+        self.laskuri += k
+ 
     def get(self, i):
-        if i == 0:
-            return self.t[0]
-        else:
-            tulos = 0
-            for k in range(0, len(self.t)):
-                siirra = self.t.popleft()
-                if k == i-1:
-                    tulos = self.t[0]
-                self.t.append(siirra)
-        
-        return tulos
-
+        indeksi = i + self.laskuri
+        indeksi = indeksi % len(self.t)
+        return self.t[indeksi]
+ 
 if __name__ == "__main__":
     q = QuickList([1,2,3,4,5,6,7,8,9,10])
     print(q.get(4)) # 5
@@ -40,7 +32,12 @@ if __name__ == "__main__":
     q.move(7)
     q.move(5)
     print(q.get(0)) # 9
-    #q = QuickList(list(range(1, 10**5 + 1)))
-    #for i in range(10**5 - 1):
-     #   q.move(10**5 - 1)
-    #print(q.get(10**4))
+    q = QuickList(list(range(1, 10**5 + 1)))
+    for i in range(10**5 - 1):
+       q.move(10**5 - 1)
+    print(q.get(10**4))
+    print(q.get(0))
+    q = QuickList(list(range(1, 10**5 + 1)))
+    for i in range(5*10**4):
+        q.move(2*i + 1)
+        print(q.get(10**5 - 1 - i))
