66f497e3cbc78c81ef111c8e61231555b30ea4fb
jussiiih <107620116+jussiiih@users.noreply.github.com>
2023-01-27 15:42:44 +0200

    Update robot.py

--- robot.py
+++ robot.py
@@ -1,3 +1,9 @@
+#TEHTÄVÄNANTO
+#Robotti on alussa ruudussa (0,0). Tämän jälkeen robotti liikkuu annetun liikesarjan mukaisesti askeleen kerrallaan.
+#Liikesarja muodostuu merkeistä U (up), D (down), L (left) ja R (right). Monessako eri ruudussa robotti käy yhteensä?
+#Voit olettaa, että liikesarjassa on enintään 105 komentoa.
+#Toteuta tiedostoon robot.py funktio count, jolle annetaan robotin liikesarja ja joka ilmoittaa eri ruutujen määrän.
+
 def count(s):
     x = 0
     y = 0
