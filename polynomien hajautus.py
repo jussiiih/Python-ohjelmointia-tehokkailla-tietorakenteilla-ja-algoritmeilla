88e73957657924c6d7261157d3b86bc51e3047f7
jussiiih <107620116+jussiiih@users.noreply.github.com>
2023-01-27 15:41:54 +0200

    Update polynomien hajautus.py

--- polynomien hajautus.py
+++ polynomien hajautus.py
@@ -1,3 +1,14 @@
+#TEHTÄVÄNANTO
+#Tarkastellaan kurssikirjan mukaista polynomista hajautusta, jossa A=19 ja modulo N=106. Tällöin esimerkiksi merkkijonon aybabtu hajautusarvo on 532916.
+#Laske hajautusarvo seuraaville merkkijonoille:
+#maanantai
+#tiistai
+#keskiviikko
+#torstai
+#perjantai
+#lauantai
+#sunnuntai
+
 def hajautus (mjono):
     tulos = 0
     kerroin = len(mjono)-1
