5c6286c34ca8f17950fdb49ae38e504715abbf12
jussiiih <107620116+jussiiih@users.noreply.github.com>
2023-01-27 15:54:20 +0200

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -1,3 +1,13 @@
+#TEHTÄVÄNANTO
+#Tehtäväsi on toteuttaa tietorakenne, joka pitää yllä tehokkaasti työlistaa.
+#Listalle voi lisätä uuden tehtävän, jolla on tietty nimi ja prioriteetti, sekä listalta voi hakea käsittelyyn suurimman prioriteetin tehtävän.
+#Voit olettaa, että työtehtävän nimi muodostuu merkeistä a–z ja 0–9 ja prioriteetti on kokonaisluku välillä 0…109.
+
+#Toteuta tiedostoon tasks.py luokka Tasks, jossa on seuraavat funktiot:
+#add: lisää listalle työtehtävän, jolla on tietty nimi ja prioriteetti
+#next: poistaa listalta korkeimman prioriteetin työtehtävän ja antaa sen nimen (jos vaihtoehtoja on monta, valitaan aakkosjärjestyksessä ensimmäinen)
+#Kummankin funktion tulee toimia tehokkaasti.
+
 from heapq import heappush, heappop
 
 class Tasks:
