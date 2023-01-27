7e9b5b32d4d0e748b023cda1bf48cf8c92bfd26a
jussiiih <107620116+jussiiih@users.noreply.github.com>
2023-01-27 14:56:29 +0200

    Update encryption.py

--- encryption.py
+++ encryption.py
@@ -1,3 +1,9 @@
+#TERHTÄVÄNANTO
+#Tehtäväsi on salata annettu merkkijono niin, että ensimmäinen merkki liikkuu aakkosissa yhden merkin eteenpäin,
+#toinen merkki kaksi merkkiä eteenpäin, kolmas merkki kolme merkkiä eteenpäin, jne. Jos merkki kasvaa suuremmaksi kuin z, se palaa taas aakkosten alkuun.
+#Merkkijono muodostuu merkeistä a–z ja siinä on enintään 100 merkkiä.
+#Toteuta tiedostoon encryption.py funktio encrypt, joka tuottaa salatun merkkijonon.
+
 def encrypt(s):
    tulos = ""
    indeksi = 1
